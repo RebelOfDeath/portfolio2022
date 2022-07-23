4f7522fbb0feda92f5a47b4f4d09cb2c7e1fdf5d
Roham <koohestaniroham@gmail.com>
2022-07-23 10:16:12 +0200

    added the hamburger menu to website
    although the hamburger menu can now be found in the DOM there is no functionality to it

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
-let cycle = document.querySelector('.cycle-through-phrases');
-let words = cycle.getAttribute('word');
+let cycle = document.querySelector(".cycle-through-phrases");
+let words = cycle.getAttribute("word");
+let hamMenu = document.querySelector(".ham-menu");
 
 words = words.split(",");
 let i = 0;
-setTimeout(function cycleWords(){
+setTimeout(function cycleWords() {
     cycle.innerHTML = words[i];
-    i = (i+1) % words.length;
+    i = (i + 1) % words.length;
     setTimeout(cycleWords, 2000);
 }, 2);
 
-document.querySelector('.screenshot-website').addEventListener('click', ()=>{
-    window.open("https://www.arduinoproject.ga/"); 
-})
+document.querySelector(".screenshot-website").addEventListener("click", () => {
+    window.open("https://www.arduinoproject.ga/");
+});
 
-document.querySelector('.mouse-outside').addEventListener('click', ()=>{
-    let rect = document.querySelector('.about-me').getBoundingClientRect();
+document.querySelector(".mouse-outside").addEventListener("click", () => {
+    let rect = document.querySelector(".about-me").getBoundingClientRect();
 
     window.scroll({
         top: rect.top + 5,
-        behavior: 'smooth'
-    })
-})
+        behavior: "smooth",
+    });
+});
+
+hamMenu.addEventListener("click", () => {
+    if (hamMenu.getAttribute("active") == "false") {
+        hamMenu.setAttribute("active", "true");
+    } else {
+        hamMenu.setAttribute("active", "false");
+    }
+});
